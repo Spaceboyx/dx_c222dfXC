840567a30970d83e9821620776a9f0d9db5623e1
Spaceboyx <38949205+Spaceboyx@users.noreply.github.com>
2018-05-03 15:16:14 +0200

    Create xSer.java

---
+++ xSer.java
@@ -0,0 +1,26 @@
+import java.net.*;
+
+
+public class xSer {
+
+    public static void main(String[] args) {
+
+        int nreq = 1;
+        try
+        {
+            ServerSocket sock = new ServerSocket (8080);
+            for (;;)
+            {
+                Socket newsock = sock.accept();
+                System.out.println("Creating thread ...");
+                Thread t = new ThreadHandler(newsock,nreq);
+                t.start();
+            }
+        }
+        catch (Exception e)
+        {
+            System.out.println("IO error " + e);
+        }
+        System.out.println("End!");
+    }
+}
